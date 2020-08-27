d1b243a4dba59ad71947915fba50ef7be08b32f3
YunuenM <54723840+YunuenM@users.noreply.github.com>
2020-08-27 17:26:44 -0400

    Create CCC '15 J1 - Special Day.cpp

---
+++ CCC/CCC '15 J1 - Special Day.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int date, month;
+    
+    cin >> month >> date;
+    if(month == 2 && date == 18){
+        cout << "Special";
+    }
+    else if(month < 2){
+        cout << "Before";
+    }
+    else if(month > 2){
+        cout << "After";
+    }
+    else if(date < 18){
+        cout << "Before";
+    }
+    else if(date > 18){
+        cout << "After";
+    }
+}
