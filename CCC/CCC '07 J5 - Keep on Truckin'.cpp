7a37176fcfc70471c3c1ac89e42c0aa1984d7283
YunuenM <54723840+YunuenM@users.noreply.github.com>
2020-08-28 17:40:35 -0400

    Update and rename CCC '17 J5 - Keep on Truckin'.cpp to CCC '07 J5 - Keep on Truckin'.cpp

--- CCC/CCC '07 J5 - Keep on Truckin'.cpp
+++ CCC/CCC '07 J5 - Keep on Truckin'.cpp
@@ -21,28 +21,28 @@ long long int solve(long long int current, long long int motel_position){
 }
 
 int main() {
-    cin.sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-		hotels.push_back(990);
-		hotels.push_back(1010);
-		hotels.push_back(1970);
-		hotels.push_back(2030);
-		hotels.push_back(2940);
-		hotels.push_back(3060);
-		hotels.push_back(3930);
-		hotels.push_back(4060);
-		hotels.push_back(4970);
-		hotels.push_back(5030);
-		hotels.push_back(5990);
-		hotels.push_back(6010);
-		hotels.push_back(7000);
-		cin >> A >> B >> N;
-		for(int i = 0; i < N; i++){
-			long long int temp;
-			cin >> temp;
-			hotels.push_back(temp);
-		}
-		sort(hotels.begin(), hotels.end());
-		cout << solve(0, 0) << endl;
+	cin.sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	hotels.push_back(990);
+	hotels.push_back(1010);
+	hotels.push_back(1970);
+	hotels.push_back(2030);
+	hotels.push_back(2940);
+	hotels.push_back(3060);
+	hotels.push_back(3930);
+	hotels.push_back(4060);
+	hotels.push_back(4970);
+	hotels.push_back(5030);
+	hotels.push_back(5990);
+	hotels.push_back(6010);
+	hotels.push_back(7000);
+	cin >> A >> B >> N;
+	for(int i = 0; i < N; i++){
+		long long int temp;
+		cin >> temp;
+		hotels.push_back(temp);
+	}
+	sort(hotels.begin(), hotels.end());
+	cout << solve(0, 0) << endl;
 }
